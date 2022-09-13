98d18a72931f08014c6193b90812a0414bf7d884
apaz <apazssd@gmail.com>
2022-09-12 19:36:57 -0500

    ejercicios enum

--- ejercicios_sesion9/EnumsWithConstructor.java
+++ ejercicios_sesion9/EnumsWithConstructor.java
@@ -1,23 +1,53 @@
 public class EnumsWithConstructor{
 	public static void main(String args[]){
 		System.out.println(DocumentType.DNI);
-		DocumentType.DNI.printDescription();//call method
-		System.out.println(DocumentType.PASS);
-	}
-	
+		DocumentType.CE.printDescription();//instance method
+		DocumentType.CE.print("static message");//static method
+		DocumentType.PASS.sumar(5);		
+		System.out.println(DocumentType.DNI.edad);
+		DocumentType.DNI.edad = 20;
+		System.out.println(DocumentType.edad);
+		System.out.println(DocumentType.DNI.edad);
+		System.out.println(DocumentType.CE.edad);
+	}	
 }
 
 enum DocumentType{
-	DNI("dni"), CE("carnet de extranjeria"),
-	PASS ("pasaporte");
+	DNI("dni"){
+		public void sumar(int op1){
+			System.out.println(op1+2);
+		}
+	}, 
+	CE("carnet de extranjeria"){
+		public void sumar(int op1){
+			System.out.println(op1+3);
+		}
+		public void printDescription(){		
+			System.out.println("CE>>"+edad);
+		}
+	},
+	PASS ("pasaporte"){
+		public void sumar(int op1){
+			System.out.println(op1+4);
+		}
+	};
 
-	private final String description;
+	//variables de instacia o estáticos
+	protected final String description;
+	public String name;
+	protected static int edad;
+	
+	// solo puede ser privado
 	private DocumentType(String description){
 		System.out.println("in constructor: "+ description);
 		this.description = description;
 	}
-	public void printDescription(){
+	public void printDescription(){		
 		System.out.println(description);
 	}
+	public static final void print(String message){		
+		System.out.println(message);
+	}
+	public abstract void sumar(int op1);
 }
 
